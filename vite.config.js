1bb9bafd5e7fe08ab1c87d89c54566f1255603be
Aleksandr Zakharov <aleksandrzakharov91@gmail.com>
2024-09-09 21:14:17 +0300

    Adds pages config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
+import { resolve } from "path";
 export default {
   base: "/elvina/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        redisign: resolve(__dirname, "redisign.html"),
+        order: resolve(__dirname, "order.html"),
+        mobile: resolve(__dirname, "mobile.html"),
+      },
+    },
+  },
 };
